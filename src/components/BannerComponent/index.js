a89eb49f16d06d30b34f0e598044d1a1acb88a58
rosemol-k-j <59862923+rosemol-k-j@users.noreply.github.com>
2022-12-19 19:33:51 +0400

    Create index.js

---
+++ src/components/BannerComponent/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function BannerComponent () {
+    return (
+        <div className="banner-component">
+            <div className="banner-text">
+            Home Shopping,<br/>
+            Your Choice!<br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br/>
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
+            </div>
+        </div>
+    )
+}
