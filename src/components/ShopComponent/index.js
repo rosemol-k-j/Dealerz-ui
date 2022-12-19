96f81fcb2fa9a4aa84491effcf3ff8d6697f7b01
rosemol-k-j <59862923+rosemol-k-j@users.noreply.github.com>
2022-12-19 19:37:22 +0400

    Shop Component

---
+++ src/components/ShopComponent/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderComponent from "../HeaderComponent";
+
+export default function ShopComponent() {
+  return (
+    <div className="container">
+      <div className="column fullWidth">
+        <HeaderComponent />
+      </div>
+    </div>
+  );
+}
